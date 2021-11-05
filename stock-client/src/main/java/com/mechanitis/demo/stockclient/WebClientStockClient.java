16d58bbd8ef4e1356ad34f0e25c4bdc98fbb9000
pkrysztofiak <p.krysztofiak@protonmail.com>
2021-11-05 06:28:14 +0100

    Create client

---
+++ stock-client/src/main/java/com/mechanitis/demo/stockclient/WebClientStockClient.java
@@ -0,0 +1,28 @@
+package com.mechanitis.demo.stockclient;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.util.retry.Retry;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Log4j2
+public class WebClientStockClient {
+
+    private WebClient webClient;
+
+    public WebClientStockClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Flux<StockPrice> pricesFor(String symbol) {
+        return webClient.get().uri("http://localhost:8080/stocks/{symbol}", symbol)
+                .retrieve()
+                .bodyToFlux(StockPrice.class)
+                .retryWhen(Retry.backoff(5, Duration.ofSeconds(5)))
+                .doOnError(IOException.class, e -> log.error(e.getMessage()))
+                ;
+    }
+}
